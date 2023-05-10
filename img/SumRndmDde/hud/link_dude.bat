35013d1aee9d2cd38c553a599bd739dad39db6e6
hermannbarbato <hermannbarbato@gmail.com>
2023-05-10 17:54:35 +0200

    Mega spostamento file
    Al fine di risolvere il problema di dimensioni ho dovuto fare dei cambiamenti, in particolare:
    - Molte grafiche usate dai plugin inutilmente sono state spostate nella cartella "unused media"
    -Il plugin RabbitMap ora rimanda a pictures, dove le immagini possono essere indicate come risorse utili, anzichè puntare alla cartella maps
    - Per lo stesso motivo, la cartella di SumRNDMDDe rimanda tramite linking a pictures (non funziona senza linking)

---
+++ img/SumRndmDde/hud/link_dude.bat
@@ -0,0 +1,5 @@
+echo "Script di configurazione link simbolici"
+echo "Versione Windows, in caso di errore per 'percorso non trovato' editare il file sostituendo il percorso del progetto"
+mklink /D  "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-media-lib\img\SumRndmDde\hud\pictures"  "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-media-lib\img\pictures" 
+pause
+echo "OK, ho fatto"
